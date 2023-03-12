6e407ee36e7398b9ae432c8128a35c6f6d638f9a
userlan <raushangul714@gmail.com>
2023-03-12 14:07:29 +0600

    15_introducing_parameters done

--- test_add_group.py
+++ test_add_group.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 import unittest
 
 def is_alert_present(wd):
