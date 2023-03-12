501efac5569a5c7741f4114e510b96f1af0a011b
userlan <raushangul714@gmail.com>
2023-03-12 21:13:23 +0600

    2.3 decomposition: Выделен класс для работы с группами для test_add_group (fixture\group)

--- fixture/session.py
+++ fixture/session.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
-class SessionHelper():
-    def __init__(self,app):
+class SessionHelper:
+    def __init__(self, app):
         self.app = app
 
     def login(self, username, password):
