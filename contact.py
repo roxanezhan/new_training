c1c80c7a3974e87ab563eaefa493a41fd9352c60
userlan <raushangul714@gmail.com>
2023-03-12 17:34:30 +0600

    DZ3: all parametrs done except photo

--- contact.py
+++ contact.py
@@ -2,7 +2,28 @@
 class Contact:
 
 
-    def __init__(self, firstname, middlename, lastname):
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home_phone, mobile_phone, work_phone, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear, address2, phone2, notes):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
--- test_add_contact.py
+++ test_add_contact.py
@@ -22,7 +22,7 @@ class test_add_contact(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.new_contact(wd, Contact(firstname="f_name", middlename="m_name", lastname="l_name"))
+        self.new_contact(wd, Contact(firstname="f_name", middlename="m_name", lastname="l_name", nickname="nick_name", title="title", company="company", address="address, qqq", home_phone="12-12-12", mobile_phone="111-121-12-12", work_phone="23-23-23", fax="34-34-34", email="111@222.qq", email2="222@222.qq", email3="333@222.qq", homepage="123.kz", bday="6", bmonth="April", byear="1973", aday="1", amonth="January", ayear="2000", address2="address, www", phone2="123", notes="qwerty"))
         self.logout(wd)
 
     def logout(self, wd):
@@ -35,6 +35,29 @@ class test_add_contact(unittest.TestCase):
         wd.find_element(By.NAME, "firstname").send_keys(contact.firstname)
         wd.find_element(By.NAME, "middlename").send_keys(contact.middlename)
         wd.find_element(By.NAME, "lastname").send_keys(contact.lastname)
+        wd.find_element(By.NAME, "nickname").send_keys(contact.nickname)
+        #wd.find_element(By.NAME, "photo").send_keys(contact.photo)
+        wd.find_element(By.NAME, "title").send_keys(contact.title)
+        wd.find_element(By.NAME, "company").send_keys(contact.company)
+        wd.find_element(By.NAME, "address").send_keys(contact.address)
+        wd.find_element(By.NAME, "home").send_keys(contact.home_phone)
+        wd.find_element(By.NAME, "mobile").send_keys(contact.mobile_phone)
+        wd.find_element(By.NAME, "work").send_keys(contact.work_phone)
+        wd.find_element(By.NAME, "fax").send_keys(contact.fax)
+        wd.find_element(By.NAME, "email").send_keys(contact.email)
+        wd.find_element(By.NAME, "email2").send_keys(contact.email2)
+        wd.find_element(By.NAME, "email3").send_keys(contact.email3)
+        wd.find_element(By.NAME, "homepage").send_keys(contact.homepage)
+        Select(wd.find_element(By.NAME, "bday")).select_by_visible_text(contact.bday)
+        Select(wd.find_element(By.NAME, "bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element(By.NAME, "byear").send_keys(contact.byear)
+        Select(wd.find_element(By.NAME, "aday")).select_by_visible_text(contact.aday)
+        Select(wd.find_element(By.NAME, "amonth")).select_by_visible_text(contact.amonth)
+        wd.find_element(By.NAME, "ayear").send_keys(contact.ayear)
+        wd.find_element(By.NAME, "address2").send_keys(contact.address2)
+        wd.find_element(By.NAME, "phone2").send_keys(contact.phone2)
+        wd.find_element(By.NAME, "notes").send_keys(contact.notes)
+        # wd.find_element(By.XPATH, "(//input[@name='submit'])[2]").click()
         # submit contact createion
         wd.find_element(By.NAME,"submit").click()
 
