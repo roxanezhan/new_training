a111e73ee7cd0bf0521dbe20a8bc551e78a3b6ca
userlan <raushangul714@gmail.com>
2023-03-12 13:51:12 +0600

    1.12 first_test_scenario on unittest

---
+++ test_add_group.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import unittest
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to().alert
+        return True
+    except:
+        return False
+
+class TestAddGroup(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(60)
+
+    def test_add_group(self):
+        success = True
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+        wd.find_element(By.NAME, "user").click()
+        wd.find_element(By.NAME, "user").clear()
+        wd.find_element(By.NAME, "user").send_keys("admin")
+        wd.find_element(By.NAME, "pass").click()
+        wd.find_element(By.NAME, "pass").clear()
+        wd.find_element(By.NAME, "pass").send_keys("secret")
+        wd.find_element(By.XPATH, "//input[@value='Login']").click()
+        wd.find_element(By.LINK_TEXT, "groups").click()
+        wd.find_element(By.NAME, "new").click()
+        wd.find_element(By.NAME, "group_name").click()
+        wd.find_element(By.NAME, "group_name").clear()
+        wd.find_element(By.NAME, "group_name").send_keys("testgroup")
+        wd.find_element(By.NAME, "group_header").click()
+        wd.find_element(By.NAME, "group_header").clear()
+        wd.find_element(By.NAME, "group_header").send_keys("testheader")
+        wd.find_element(By.NAME, "group_footer").click()
+        wd.find_element(By.NAME, "group_footer").clear()
+        wd.find_element(By.NAME, "group_footer").send_keys("testfooter")
+        wd.find_element(By.NAME, "submit").click()
+        wd.find_element(By.LINK_TEXT, "group page").click()
+        wd.find_element(By.LINK_TEXT, "Logout").click()
+        self.assertTrue(success)
+
+    def tearDown(self):
+        self.wd.quit()
