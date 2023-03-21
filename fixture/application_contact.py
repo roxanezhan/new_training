b7880963f4faa95dddf34d451e1d92c97a4a53da
userlan <raushangul714@gmail.com>
2023-03-21 13:53:37 +0600

    2.3 decomposition: Выделен класс для работы с группами для test_add_contact (fixture\contact)

--- fixture/application_contact.py
+++ fixture/application_contact.py
@@ -1,8 +1,7 @@
 # этот скрипт пишет разработчик по вспомогательным методам
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 from fixture.session import SessionHelper
+from fixture.contact import ContactHelper
 
 
 class Application_contact:
@@ -10,46 +9,11 @@ class Application_contact:
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
-
-    def new_contact(self, contact):
-        wd = self.wd
-        # init contact createion
-        wd.find_element(By.LINK_TEXT, "add new").click()
-        # fill contact form
-        wd.find_element(By.NAME, "firstname").send_keys(contact.firstname)
-        wd.find_element(By.NAME, "middlename").send_keys(contact.middlename)
-        wd.find_element(By.NAME, "lastname").send_keys(contact.lastname)
-        wd.find_element(By.NAME, "nickname").send_keys(contact.nickname)
-        wd.find_element(By.XPATH, "//input[@name='photo']").send_keys(contact.photo)
-        wd.find_element(By.NAME, "title").send_keys(contact.title)
-        wd.find_element(By.NAME, "company").send_keys(contact.company)
-        wd.find_element(By.NAME, "address").send_keys(contact.address)
-        wd.find_element(By.NAME, "home").send_keys(contact.home_phone)
-        wd.find_element(By.NAME, "mobile").send_keys(contact.mobile_phone)
-        wd.find_element(By.NAME, "work").send_keys(contact.work_phone)
-        wd.find_element(By.NAME, "fax").send_keys(contact.fax)
-        wd.find_element(By.NAME, "email").send_keys(contact.email)
-        wd.find_element(By.NAME, "email2").send_keys(contact.email2)
-        wd.find_element(By.NAME, "email3").send_keys(contact.email3)
-        wd.find_element(By.NAME, "homepage").send_keys(contact.homepage)
-        Select(wd.find_element(By.NAME, "bday")).select_by_visible_text(contact.bday)
-        Select(wd.find_element(By.NAME, "bmonth")).select_by_visible_text(contact.bmonth)
-        wd.find_element(By.NAME, "byear").send_keys(contact.byear)
-        Select(wd.find_element(By.NAME, "aday")).select_by_visible_text(contact.aday)
-        Select(wd.find_element(By.NAME, "amonth")).select_by_visible_text(contact.amonth)
-        wd.find_element(By.NAME, "ayear").send_keys(contact.ayear)
-        wd.find_element(By.NAME, "address2").send_keys(contact.address2)
-        wd.find_element(By.NAME, "phone2").send_keys(contact.phone2)
-        wd.find_element(By.NAME, "notes").send_keys(contact.notes)
-        # submit contact createion
-        wd.find_element(By.NAME,"submit").click()
+        self.contact = ContactHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
-    def tearDown(self):
-        self.wd.quit()
 
     def destroy(self):
         self.wd.quit()
---
+++ fixture/contact.py
@@ -0,0 +1,41 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def add_new(self, contact):
+        wd = self.app.wd
+        # init contact createion
+        wd.find_element(By.LINK_TEXT, "add new").click()
+        # fill contact form
+        wd.find_element(By.NAME, "firstname").send_keys(contact.firstname)
+        wd.find_element(By.NAME, "middlename").send_keys(contact.middlename)
+        wd.find_element(By.NAME, "lastname").send_keys(contact.lastname)
+        wd.find_element(By.NAME, "nickname").send_keys(contact.nickname)
+        wd.find_element(By.XPATH, "//input[@name='photo']").send_keys(contact.photo)
+        wd.find_element(By.NAME, "title").send_keys(contact.title)
+        wd.find_element(By.NAME, "company").send_keys(contact.company)
+        wd.find_element(By.NAME, "address").send_keys(contact.address)
+        wd.find_element(By.NAME, "home").send_keys(contact.home_phone)
+        wd.find_element(By.NAME, "mobile").send_keys(contact.mobile_phone)
+        wd.find_element(By.NAME, "work").send_keys(contact.work_phone)
+        wd.find_element(By.NAME, "fax").send_keys(contact.fax)
+        wd.find_element(By.NAME, "email").send_keys(contact.email)
+        wd.find_element(By.NAME, "email2").send_keys(contact.email2)
+        wd.find_element(By.NAME, "email3").send_keys(contact.email3)
+        wd.find_element(By.NAME, "homepage").send_keys(contact.homepage)
+        Select(wd.find_element(By.NAME, "bday")).select_by_visible_text(contact.bday)
+        Select(wd.find_element(By.NAME, "bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element(By.NAME, "byear").send_keys(contact.byear)
+        Select(wd.find_element(By.NAME, "aday")).select_by_visible_text(contact.aday)
+        Select(wd.find_element(By.NAME, "amonth")).select_by_visible_text(contact.amonth)
+        wd.find_element(By.NAME, "ayear").send_keys(contact.ayear)
+        wd.find_element(By.NAME, "address2").send_keys(contact.address2)
+        wd.find_element(By.NAME, "phone2").send_keys(contact.phone2)
+        wd.find_element(By.NAME, "notes").send_keys(contact.notes)
+        # submit contact createion
+        wd.find_element(By.NAME,"submit").click()
