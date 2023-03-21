7535af6b1635fa1e821927f2f37c29d8b664ba7a
userlan <raushangul714@gmail.com>
2023-03-22 00:23:30 +0600

    3.3_login done: проверка на вставленную по ошибке logout в test_add_group.py не работает

--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,7 @@ from fixture.contact import ContactHelper
 class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
@@ -18,6 +18,7 @@ class Application:
             return True
         except:
             return False
+
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
