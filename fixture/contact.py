d9d8818df5e70eda58f9d2336b7abab5a7f597d4
userlan <raushangul714@gmail.com>
2023-05-10 19:22:53 +0600

    5_2 splitting strings. Modified: fixture\contact.py. Added: test\test_phones.py

--- fixture/contact.py
+++ fixture/contact.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from model.contact import Contact
 import time
+import re
 
 
 class ContactHelper:
@@ -27,10 +28,10 @@ class ContactHelper:
 
     def delete_contact_by_index(self, index):
         wd = self.app.wd
-        #wd.find_element(By.LINK_TEXT, "home").click()
+        # wd.find_element(By.LINK_TEXT, "home").click()
         self.open_contact_page()
         # select first contact
-        #wd.find_element(By.NAME, "selected[]").click()
+        # wd.find_element(By.NAME, "selected[]").click()
         self.select_contact_by_index(index)
         # submit deletion
         wd.find_element(By.XPATH, "//input[@value='Delete']").click()
@@ -47,7 +48,7 @@ class ContactHelper:
         # fill contact form
         self.fill_contact_form(contact)
         # submit contact createion
-        wd.find_element(By.NAME,"submit").click()
+        wd.find_element(By.NAME, "submit").click()
         self.contact_cache = None
 
     def count(self):
@@ -55,29 +56,13 @@ class ContactHelper:
         self.open_contact_page()
         return len(wd.find_elements(By.NAME, "selected[]"))
 
-    contact_cache = None
-
-    def get_contact_list(self):
-        if self.contact_cache is None:
-            wd = self.app.wd
-            self.open_contact_page()
-            self.contact_cache = []
-            for element in wd.find_elements(By.CSS_SELECTOR, "tr[name='entry']"):
-                id = element.find_element(By.NAME, "selected[]").get_attribute("value")
-                #text = element.text
-                l_name = element.find_element(By.CSS_SELECTOR, "td:nth-child(2)").text
-                f_name = element.find_element(By.CSS_SELECTOR, "td:nth-child(3)").text
-                self.contact_cache.append(Contact(id=id, firstname=f_name, lastname=l_name))
-                #print("here is id: ", id)                #print("here is firstname:", f_name)                #print("here is lastname:", l_name)
-        return list(self.contact_cache)
-
     def modify_first_contact(self):
         self.modify_contact_by_index(0)
 
     def modify_contact_by_index(self, index, new_contact_data):
         wd = self.app.wd
         self.open_contact_page()
-        #self.select_contact_by_index(index)
+        # self.select_contact_by_index(index)
         # open modification form
         wd.find_elements(By.XPATH, "//img[@alt='Edit']")[index].click()
         # fill contact form
@@ -95,7 +80,7 @@ class ContactHelper:
         if contact.photo:
             wd.find_element(By.XPATH, "//input[@name='photo']").clear()
             wd.find_element(By.XPATH, "//input[@name='photo']").send_keys(contact.photo)
-        #self.change_field_value("//input[@name='photo']", contact.photo)
+        # self.change_field_value("//input[@name='photo']", contact.photo)
         self.change_field_value("title", contact.title)
         self.change_field_value("company", contact.company)
         self.change_field_value("address", contact.address)
@@ -107,17 +92,17 @@ class ContactHelper:
         self.change_field_value("email2", contact.email2)
         self.change_field_value("email3", contact.email3)
         self.change_field_value("homepage", contact.homepage)
-        #Select(wd.find_element(By.NAME, "bday")).select_by_visible_text(contact.bday)
+        # Select(wd.find_element(By.NAME, "bday")).select_by_visible_text(contact.bday)
         if contact.bday:
             Select(wd.find_element(By.NAME, "bday")).select_by_visible_text(contact.bday)
-        #Select(wd.find_element(By.NAME, "bmonth")).select_by_visible_text(contact.bmonth)
+        # Select(wd.find_element(By.NAME, "bmonth")).select_by_visible_text(contact.bmonth)
         if contact.bmonth:
             Select(wd.find_element(By.NAME, "bmonth")).select_by_visible_text(contact.bmonth)
         self.change_field_value("byear", contact.byear)
-        #Select(wd.find_element(By.NAME, "aday")).select_by_visible_text(contact.aday)
+        # Select(wd.find_element(By.NAME, "aday")).select_by_visible_text(contact.aday)
         if contact.aday:
             Select(wd.find_element(By.NAME, "aday")).select_by_visible_text(contact.aday)
-        #Select(wd.find_element(By.NAME, "amonth")).select_by_visible_text(contact.amonth)
+        # Select(wd.find_element(By.NAME, "amonth")).select_by_visible_text(contact.amonth)
         if contact.amonth:
             Select(wd.find_element(By.NAME, "amonth")).select_by_visible_text(contact.amonth)
         self.change_field_value("ayear", contact.ayear)
@@ -131,3 +116,61 @@ class ContactHelper:
             wd.find_element(By.NAME, field_name).click()
             wd.find_element(By.NAME, field_name).clear()
             wd.find_element(By.NAME, field_name).send_keys(text)
+
+    contact_cache = None
+
+    def get_contact_list(self):
+        if self.contact_cache is None:
+            wd = self.app.wd
+            self.open_contact_page()
+            self.contact_cache = []
+            for element in wd.find_elements(By.NAME, "entry"):
+                id = element.find_element(By.NAME, "selected[]").get_attribute("value")
+                #print("\nthe id is:", id)
+                cells = element.find_elements(By.TAG_NAME, "td")
+                firstname = cells[2].text
+                #print("\nthis is firstname:", firstname, "\nfirstnameend")
+                lastname = cells[1].text
+                #print("\nthis is lastname:", lastname, "\nlastnameend")
+                all_phones = cells[5].text.splitlines()
+                self.contact_cache.append(Contact(id=id, firstname=firstname, lastname=lastname, home_phone=all_phones[0], mobile_phone=all_phones[1],
+                                                  work_phone=all_phones[2], phone2=all_phones[3]))
+        return list(self.contact_cache)
+
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        row = wd.find_elements(By.NAME, "entry")[index]
+        cell = row.find_elements(By.TAG_NAME, "td")[7]
+        cell.find_element(By.TAG_NAME, "a").click()
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        row = wd.find_elements(By.NAME, "entry")[index]
+        cell = row.find_elements(By.TAG_NAME, "td")[6]
+        cell.find_element(By.TAG_NAME, "a").click()
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element(By.NAME, "firstname").get_attribute("value")
+        lastname = wd.find_element(By.NAME, "lastname").get_attribute("value")
+        id = wd.find_element(By.NAME, "id").get_attribute("value")
+        homephone = wd.find_element(By.NAME, "home").get_attribute("value")
+        workphone = wd.find_element(By.NAME, "work").get_attribute("value")
+        mobilephone = wd.find_element(By.NAME, "mobile").get_attribute("value")
+        phone2 = wd.find_element(By.NAME, "phone2").get_attribute("value")
+        return Contact(firstname=firstname, lastname=lastname, id=id,
+                       home_phone=homephone, mobile_phone=mobilephone,
+                       work_phone=workphone, phone2=phone2)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element(By.ID, "content").text
+        homephone = re.search("H: (.*)", text).group(1)
+        workphone = re.search("W: (.*)", text).group(1)
+        mobilephone = re.search("M: (.*)", text).group(1)
+        phone2 = re.search("P: (.*)", text).group(1)
+        return Contact(home_phone=homephone, mobile_phone=mobilephone, work_phone=workphone, phone2=phone2)
