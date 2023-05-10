2774551659334de46723298e0b2e3091409a2788
userlan <raushangul714@gmail.com>
2023-05-11 00:23:52 +0600

    5_5 options. Modified: 1) fixture\application.py (изменен метод __init__), 2) conftest.py (изменен метод app, создан метод pytest_addoption)

--- fixture/application.py
+++ fixture/application.py
@@ -3,14 +3,25 @@ from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
+#from webdriver_manager.chrome import ChromeDriverManager
 
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+            #service = ChromeService(executable_path=ChromeDriverManager().install())
+            #self.wd = webdriver.Chrome(service=service)
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser) #raise - означает выполнение кода будет аварийно прервано
         #self.wd.implicitly_wait(2)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -21,7 +32,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
