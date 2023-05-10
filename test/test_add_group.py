75ff6033605a809476f865f85e3617e7205668f9
userlan <raushangul714@gmail.com>
2023-05-10 21:45:58 +0600

    5_4 Data Driven Test. Modified: test\test_add_group.py (создан метод random_string и список testdata), model\group.py (изменен repr)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,22 +1,34 @@
 # этот скрипт пишет разработчик по тестам
-
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import random
+import string
 
-def test_add_group(app):
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10 #генератор случайных символов в строке, где " "*10 означает увеличение частоты пробелов в 10 раз
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]) #генератор случайных строк
+
+#сначала проверяется тест на пустых данных, потом цикл из случайно сгенерированнх полей:
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name", 10), header=random_string("header", 10), footer=random_string("footer", 10))
+    for i in range(5)
+]
+
+#чередование пустых и непустых полей в тесте с использованием list comprehension. На выходе будет 8 возможных вариантов:
+#testdata = [
+#    Group(name=name, header=header, footer=footer)
+#    for name in ["", random_string("name", 10)] #цикл for пробегает по 2м возможным значением (пустое и случайное)
+#    for header in ["", random_string("header", 20)]
+#    for footer in ["", random_string("footer", 20)]
+#]
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="testgroup", header="testheader", footer="testfooter")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()   #count() выступает в роли хеш функции
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-#def test_add_empty_group(app):
-#    old_groups = app.group.get_group_list()
-#    group = Group(name="", header="", footer="")
-#    app.group.create(group)
-#    new_groups = app.group.get_group_list()
-#    assert len(old_groups) + 1 == len(new_groups)
-#    old_groups.append(group)
-#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
