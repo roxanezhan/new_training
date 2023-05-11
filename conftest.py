427c026d696021842d64fa7c233f65f3fbb88136
userlan <raushangul714@gmail.com>
2023-05-11 20:26:36 +0600

    ДЗ 16 done. Modified: fixture\application.py, conftest.py. Для запуска из командной строки: py.test --browser=chrome --user=admin --password=secret test\test_del_group.py

--- conftest.py
+++ conftest.py
@@ -8,12 +8,14 @@ def app(request):
     global fixture
     browser = request.config.getoption("--browser")
     base_url = request.config.getoption("--baseUrl")
+    user = request.config.getoption("--user")
+    userpassword = request.config.getoption("--password")
     if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
+        fixture = Application(browser=browser, base_url=base_url, user=user, userpassword=userpassword)
     else:
         if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+            fixture = Application(browser=browser, base_url=base_url, user=user, userpassword=userpassword)
+    fixture.session.ensure_login(username=user, password=userpassword)
     return fixture
 
 
@@ -28,3 +30,5 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--user", action="store")
+    parser.addoption("--password", action="store")
