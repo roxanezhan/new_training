919457f872b17a05cc7189689a767b2f65a2a38e
userlan <raushangul714@gmail.com>
2023-05-13 00:40:23 +0600

    ДЗ 18: Реализовать генераторы тестовых данных для контактов. Created: generator\contact.py. Autogenerated: by deafult - contacts.json, if test parametrized - testc.json.

---
+++ generator/contact.py
@@ -0,0 +1,60 @@
+import jsonpickle
+from model.contact import Contact
+import random
+import string
+import os.path
+import getopt   #для чтения опций командной строки
+import sys      #для того, чтобы получить доступ к опциям командной строки
+
+#выполнение генератора данных параметризованным.
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5   #это дефолтные значения
+f = "data/contacts.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*2 #генератор случайных символов в строке, где " "*10 означает увеличение частоты пробелов в 10 раз
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]) #генератор случайных строк
+
+def random_numbers(maxlen):
+    numbers = string.digits + "-"*3
+    return "".join(
+        [random.choice(numbers) for i in range(maxlen)])  # генератор случайных числовых последовательностей
+
+def random_emails_homepage(prefix, postfix, maxlen):
+    symbolsemail = string.ascii_letters + string.digits #генератор случайных символов в строке, где " "*10 означает увеличение частоты пробелов в 10 раз
+    return prefix + "".join(
+        [random.choice(symbolsemail) for i in range(random.randrange(maxlen))]) + postfix # генератор случайных строк
+
+#сначала проверяется тест на пустых данных, потом цикл из случайно сгенерированнх полей:
+testdata_C = [Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10),
+                      lastname=random_string("lastname", 10), nickname=random_string("nickname", 10),
+                      title=random_string("title", 10), company=random_string("company", 10),
+                      address=random_string("address", 10), home_phone=random_numbers(10),
+                      mobile_phone=random_numbers(10), work_phone=random_numbers(10), fax=random_numbers(10),
+                      email=random_emails_homepage("email", "@gmail.kz", 10),
+                      email2=random_emails_homepage("email2", "@gmail.kz", 10),
+                      email3=random_emails_homepage("email3", "@gmail.kz", 10),
+                      homepage=random_emails_homepage("page", ".kz", 10), bday=random.choice('123456789'),
+                      bmonth="April", byear="1973", aday=random.choice('123456789'), amonth="January",
+                      ayear="2000", address2=random_string("address2", 10),
+                      phone2=random_numbers(10), notes=random_string("notes", 10), photo="C:\\GitProjetcs\\new_training\\icons\\camomille.jpg")
+    for i in range(n)
+]
+
+#file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/groups.json") #мы сейчас находимся в пакете genetaror,
+# а нам нужно перейти в пакет data. Для этого переходим на 1 уровень вверх в родительскую директорией командой "../".
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f) # и создание параметризованного названия файла
+with open(file, "w") as out:
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata_C))
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,36 +4,6 @@ import random
 import string
 import pytest
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*2 #генератор случайных символов в строке, где " "*10 означает увеличение частоты пробелов в 10 раз
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]) #генератор случайных строк
-
-def random_numbers(maxlen):
-    numbers = string.digits + "-"*3
-    return "".join(
-        [random.choice(numbers) for i in range(maxlen)])  # генератор случайных числовых последовательностей
-
-def random_emails_homepage(prefix, postfix, maxlen):
-    symbolsemail = string.ascii_letters + string.digits #генератор случайных символов в строке, где " "*10 означает увеличение частоты пробелов в 10 раз
-    return prefix + "".join(
-        [random.choice(symbolsemail) for i in range(random.randrange(maxlen))]) + postfix # генератор случайных строк
-
-#сначала проверяется тест на пустых данных, потом цикл из случайно сгенерированнх полей:
-testdata_C = [Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10),
-                      lastname=random_string("lastname", 10), nickname=random_string("nickname", 10),
-                      title=random_string("title", 10), company=random_string("company", 10),
-                      address=random_string("address", 10), home_phone=random_numbers(10),
-                      mobile_phone=random_numbers(10), work_phone=random_numbers(10), fax=random_numbers(10),
-                      email=random_emails_homepage("email", "@gmail.kz", 10),
-                      email2=random_emails_homepage("email2", "@gmail.kz", 10),
-                      email3=random_emails_homepage("email3", "@gmail.kz", 10),
-                      homepage=random_emails_homepage("page", ".kz", 10), bday=random.choice('123456789'),
-                      bmonth="April", byear="1973", aday=random.choice('123456789'), amonth="January",
-                      ayear="2000", address2=random_string("address2", 10),
-                      phone2=random_numbers(10), notes=random_string("notes", 10), photo="C:\\GitProjetcs\\new_training\\icons\\camomille.jpg")
-    for i in range(5)
-]
-
 @pytest.mark.parametrize("contact", testdata_C, ids=[repr(x) for x in testdata_C])
 def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
