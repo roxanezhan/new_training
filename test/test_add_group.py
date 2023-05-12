4779f04635a4d3dc71a33e31dc705271aebc00c6
userlan <raushangul714@gmail.com>
2023-05-12 21:44:12 +0600

    6.3 DDT: Загрузка тестовых данных из python-файла. Created package data and python-file data\add_group.py. Modified: test_add_group.py.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,27 +2,8 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
-import random
-import string
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10 #генератор случайных символов в строке, где " "*10 означает увеличение частоты пробелов в 10 раз
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]) #генератор случайных строк
-
-#сначала проверяется тест на пустых данных, потом цикл из случайно сгенерированнх полей:
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 10), footer=random_string("footer", 10))
-    for i in range(5)
-]
-
-#чередование пустых и непустых полей в тесте с использованием list comprehension. На выходе будет 8 возможных вариантов:
-#testdata = [
-#    Group(name=name, header=header, footer=footer)
-#    for name in ["", random_string("name", 10)] #цикл for пробегает по 2м возможным значением (пустое и случайное)
-#    for header in ["", random_string("header", 20)]
-#    for footer in ["", random_string("footer", 20)]
-#]
+#from data.add_group import testdata
+from data.add_group import constant as testdata #удобно, если нужно использовать фиксированные значения и при этом эту константу можно переименовать
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
