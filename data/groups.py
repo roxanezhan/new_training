488617bd900316d2649790f72eb01e8a89740934
userlan <raushangul714@gmail.com>
2023-05-12 23:35:52 +0600

    6.5 Dynamic test generation. Renamed data\add_group.py to data\groups.py. Modified: conftest.py, test_add_group.py.

---
+++ data/groups.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from model.group import Group
+
+testdata = [
+    Group(name="constantname1", header="constantheader1", footer="constantfooter1"),
+    Group(name="constantname2", header="constantheader2", footer="constantfooter2")
+]
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,12 +1,10 @@
 # этот скрипт пишет разработчик по тестам
 # -*- coding: utf-8 -*-
 from model.group import Group
-import pytest
-#from data.add_group import testdata
-from data.add_group import constant as testdata #удобно, если нужно использовать фиксированные значения и при этом эту константу можно переименовать
 
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group):
+
+def test_add_group(app, data_groups):
+    group = data_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()   #count() выступает в роли хеш функции
